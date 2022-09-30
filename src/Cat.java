2f9ca964118a24c3797c6708d73b6334d3e1e92e
Timur Beksultan <timurb2@nycstudents.net>
2022-09-30 08:17:17 -0400

    Initial commit

---
+++ src/Cat.java
@@ -0,0 +1,26 @@
+public class Cat {
+
+    // instance variables
+    private String name;
+    private int age;
+    private double weight;
+
+    // constructor
+    public Cat(String catName, int catAge, double catWeight) {
+        name = catName;
+        age = catAge;
+        weight = catWeight;
+    }
+
+    // method that introduces the Cat
+    public void introduce() {
+        System.out.println("Hello my name is " + name);
+    }
+
+    // method that prints Cat info
+    public void printCatInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Weight: " + weight);
+    }
+}
---
+++ src/CatRunner.java
@@ -0,0 +1,17 @@
+public class CatRunner {
+    public static void main(String[] args){
+
+        Cat cat1 = new Cat("Bill", 10, 12.5);
+        cat1.introduce();
+        cat1.printCatInfo();
+
+        Cat cat2 = new Cat("Will", 1, 5.5);
+        cat2.introduce();
+        cat2.printCatInfo();
+
+
+
+
+
+    }
+}
